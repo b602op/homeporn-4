8c7bdc7821056e71477a04b4694248d4c02ef562
Eduard Ispolatov <u281094@gmail.com>
2021-10-18 00:15:15 +0300

    #yandex add reduce

--- solution/index.js
+++ solution/index.js
@@ -1,6 +1,4 @@
-// solution/index.js
-
-export default (Homework) => async (array, fn, initialValue, cb) => {
+module.exports = (Homework) => async (array, fn, initialValue, cb) => {
     const promiseWrapper = (callback) => (...props) => new Promise(
         (resolve) => callback(...props, (result) => resolve(result)));
 
@@ -21,41 +19,3 @@ export default (Homework) => async (array, fn, initialValue, cb) => {
 
     cb(accumulator)
 }
-
-/*
-Пример
-
-const asyncArray = new Homework.AsyncArray([1, 2, 3, 4])
-const reducerSum = (acc, curr, i, src, cb) => Homework.add(acc, curr, cb)
-
-reduce(asyncArray, reducerSum, 0, (res) => {
-    console.log(res) // 10
-})
-
----------------------
-
-Сигнатура функции
-
-type reduce = (
-    array: AsyncArray
-    fn: (
-        acc: any,
-        cur: any,
-        i: number,
-        src: AsyncArray,
-        cb: (result: any) => void
-    ) => void
-    initialValue: any
-    cb: (result: any) => void
-) => void
-
-----------------------
-
-Решение должно работать на Node.js 16
-Запрещено использовать:
-Арифметические операции и операции сравнения. Вместо них следует вызывать функции из Homework
-Любые операции для работы с массивами. Доступны только методы AsyncArray
-Импорты. Весь вспомогательный код следует положить в замыкание (см. оформление)
-Комментарии в коде не проходят автотесты
-
-*/
